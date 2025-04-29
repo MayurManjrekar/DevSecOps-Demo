28a9b6835d4f57837da25e33afb98991ef471e10
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-04-29 23:21:54 +0530

    resolved all the errors

--- bookshelf/app.js
+++ bookshelf/app.js
@@ -9,12 +9,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.const awsSecret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-
-// Testing GitLeaks
-//const gcpSecret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-//const password = "mysecretpassword123";
-// Testing GitLeaks
 
 
 'use strict';
