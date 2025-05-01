d40b16aaa1ccf05b6535ae08d579ab530b657cd6
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-05-01 10:48:04 +0530

    updated lint workflow

--- bookshelf/app.js
+++ bookshelf/app.js
@@ -10,7 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 
-'use strict'
+'use strict';
 const express = require('express');
 
 const app = express();
